ba1214bd0c41e193f352cf7ffdab07fec8fea02d
maryseo070 <maryseo070@gmail.com>
2018-04-18 16:09:28 -0400

    todo delete button works

--- jql/jsfile.js
+++ jql/jsfile.js
@@ -1,7 +1,45 @@
 
-  function initialize () {
-    let $ul = $maryQuery("ul");
-    $ul.append("<li>HI</li>")
-  }
+function createTodo() {
+  $maryQuery(".addTodo").on("submit", e => {
+    e.preventDefault();
+    let todo = document.querySelectorAll(".todoBody")[0].value;
+    if (todo) {
+      $maryQuery(".todos").append(`
+        <div>
+        <li>${todo}</li>
+          <button class="deleteTodo">Delete to-do</button>
+          <input type="button" class="completed" value="Not Completed.."></input>
+        </div>
+        `);
+    }
+    document.querySelectorAll(".todoBody")[0].value = '';
+
+    $maryQuery(".deleteTodo").on("click", e => {
+      $maryQuery(e.target).parent("li").remove();
+    });
+
+    $maryQuery(".completed").on("click", e => {
+      let value = e.target.value;
+      value === "Completed!" ? e.target.setAttribute("value", "Not Completed..")  : e.target.setAttribute("value", "Completed!")
+    })
+
+  })
+}
+
+function deleteTodo(e) {
+  let target = $maryQuery(e.target)
+  $maryQuery(target).parent("li").remove();
+}
+
+function completed(e) {
+  debugger
+  let target = $maryQuery(e.target)
+  debugger
+  // targetVal === "Completed!" ? e.target.setAttribute("value", "Not Completed..")  : e.target.setAttribute("value", "Completed!");
+}
+
+function initialize () {
+  createTodo();
+}
 // $maryQuery(() => { console.log("boop") });
 $maryQuery(() => { initialize(); });
